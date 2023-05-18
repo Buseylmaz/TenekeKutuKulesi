2c5bc2692fe29aee566e551e2595bc45ee1f960f
Buse Yılmaz <bsylm@outlook.com>
2023-05-18 15:40:48 +0300

    Proje

---
+++ TenekeKutuKulesi/Assets/Script/Gecis.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class Gecis : MonoBehaviour
+{
+    
+    public void CevapAnahtari(int c)
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Oyun(int o)
+    {
+        SceneManager.LoadScene(0);
+    }
+}
---
+++ TenekeKutuKulesi/Assets/Script/Gizli.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gizli : MonoBehaviour
+{
+    Camera camera;
+    Vector2 baslangicPozisyon;
+
+
+
+    private void OnMouseDrag()
+    {
+        Vector3 pozisyon=camera.ScreenToViewportPoint(Input.mousePosition);
+        pozisyon.z = 0;
+        transform.position = pozisyon;
+    }
+
+    
+    void Start()
+    {
+        camera=GameObject.Find("Camera").GetComponent<Camera>();
+        baslangicPozisyon=transform.position;
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
